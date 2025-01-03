1459c80220941774211039265c3b5e7eb71a4c28
azdYM <02ym.azad@gmail.com>
2025-01-03 20:47:19 +0300

    feat: init game with a board

--- src/Game.ts
+++ src/Game.ts
@@ -1,3 +1,18 @@
+export class Board {
+    constructor(private x: number, private y: number = 1) {}
+    size() {
+        return this.x * this.y
+    }
+}
+
 export class Game {
-    
+    private board: Board | null = null
+
+    getBoard() {
+        return this.board
+    }
+
+    setBoard(board: Board) {
+        this.board = board
+    }
 }
--- src/game.test.ts
+++ src/game.test.ts
@@ -1,1 +1,20 @@
+import { Board, Game } from "./Game.js";
+
+test('Initialize the game without a board and return null', () => {
+    const game = new Game()
+    expect(game.getBoard()).toBeNull()
+});
+
+test('Initialize a 1D board with 5 cells', () => {
+    const game = new Game()
+    game.setBoard(new Board(5))
+    expect(game.getBoard()?.size()).toBe(5) 
+});
+
+test('Initialize a 2D board with 5x5 cells', () => {
+    const game = new Game()
+    game.setBoard(new Board(5, 5))
+    expect(game.getBoard()?.size()).toBe(25) 
+});
+
 
